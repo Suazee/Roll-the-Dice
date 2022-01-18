9c17cbe9dd2a673ef208e43c91ee4bc0b2aec284
Johnson Tolefe <96609596+Suazee@users.noreply.github.com>
2022-01-18 19:54:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,13 +9,11 @@ const ruleModal = document.querySelector(".rule-modal");
 const closeModalButton = document.querySelector(".close-modal");
 const overlay = document.querySelector(".overlay");
 
-let playerScore = 0;
+let currentScore = 0;
 let player1Total = 0;
 let player2Total = 0;
 let randomNumber;
-let playerActive;
-let playerInactive;
-let playerID;
+let activePlayer;
 
 /*---------------------------PRESENTS THE RULES OF THE GAME ONCE THE PAGE IS LOADED UP-------------------------- */
 
@@ -48,30 +46,26 @@ function whoseTurn() {
 
   for (let i = 0; i < players.length; i++) {
     if (players[i].classList.contains("player--active")) {
-      playerActive = players[i].classList[1];
-    } else {
-      playerInactive = players[i].classList[1];
+      if (players[i].classList.contains("player--0")) {
+        activePlayer = 0;
+      } else {
+        activePlayer = 1;
+      }
     }
   }
-
-  // HELPS THE GAME TO GET THE ID NUMBER OF EITHER 0 OR 1
-  //BECAUSE THE ID OF EACH PLAYER'S SCORE CARRIES EITHER A 0 OR 1 AT THE END
-
-  playerActive === "player--0" ? (playerID = 0) : (playerID = 1);
 }
 
 function updateCurrentScore() {
   // HELPS UPDATE THE CURRENT PLAYER'S SCORE ONCE THE DICE IS ROLLED
 
-  document.querySelector(`.${playerActive} #current--${playerID}`).textContent =
-    playerScore;
+  document.querySelector(`#current--${activePlayer}`).textContent =
+    currentScore;
 }
 
 function updateTotalScore(total) {
   // HELPS UPDATE THE CURRENT PLAYER'S TOTAL SCORE ONCE THE HOLD BUTTON IS PRESSED
 
-  document.querySelector(`.${playerActive} #score--${playerID}`).textContent =
-    total;
+  document.querySelector(`#score--${activePlayer}`).textContent = total;
 }
 
 function gameLogic() {
@@ -87,7 +81,7 @@ function gameLogic() {
   if (randomNumber === 1) {
     nextPlayer();
   } else {
-    playerScore += randomNumber;
+    currentScore += randomNumber;
     updateCurrentScore();
   }
 }
@@ -95,10 +89,14 @@ function gameLogic() {
 function nextPlayer() {
   // SWITCHES THE ACTIVE AND INACTIVE PLAYER AND RESETS THE CURRENT SCORE BACK TO 0
 
-  playerScore = 0;
+  currentScore = 0;
   updateCurrentScore();
-  document.querySelector(`.${playerActive}`).classList.remove("player--active");
-  document.querySelector(`.${playerInactive}`).classList.add("player--active");
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove("player--active");
+  document
+    .querySelector(`.player--${activePlayer === 0 ? 1 : 0}`)
+    .classList.add("player--active");
   whoseTurn();
   holdScoreButton.removeEventListener("click", holdScoreEvent);
 }
@@ -108,15 +106,15 @@ function holdScoreEvent() {
   // CHECKS FOR THE WINNER
   // CALLS THE nextPlayer() IF NO WINNER IS FOUND
 
-  if (playerActive === "player--0") {
-    player1Total += playerScore;
+  if (activePlayer === 0) {
+    player1Total += currentScore;
     updateTotalScore(player1Total);
     if (player1Total >= 100) {
       callWinner();
       return;
     }
   } else {
-    player2Total += playerScore;
+    player2Total += currentScore;
     updateTotalScore(player2Total);
     if (player2Total >= 100) {
       callWinner();
@@ -130,13 +128,17 @@ function holdScoreEvent() {
 function resetGame() {
   // RESETS EVERYTHING IN THE GAME BACK TO THE RELOADED PAGE VERSION
 
-  playerScore = 0;
+  currentScore = 0;
   player1Total = 0;
   player2Total = 0;
   updateCurrentScore();
   diceRolled.classList.add("hidden");
-  document.querySelector(`.${playerActive}`).classList.remove("player--winner");
-  document.querySelector(`.${playerActive}`).classList.remove("player--active");
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove("player--winner");
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove("player--active");
   document.querySelector("#score--0").textContent = player1Total;
   document.querySelector("#score--1").textContent = player2Total;
   document.querySelector(".player--0").classList.add("player--active");
@@ -147,7 +149,9 @@ function resetGame() {
 function callWinner() {
   // ADDS THE WINNER STYLE ON TO THE CURRENT PLAYER THAT HIT 100 TOTAL POINTS FIRST
 
-  document.querySelector(`.${playerActive}`).classList.add("player--winner");
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.add("player--winner");
   diceRollButton.removeEventListener("click", gameLogic);
   holdScoreButton.removeEventListener("click", holdScoreEvent);
   newGameButton.addEventListener("click", resetGame);
